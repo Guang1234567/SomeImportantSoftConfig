1e4479b803c1bd0c3d67d0bdf31ed50d8192c161
hg-li <hg-li@mygzb.com>
2017-12-02 03:08:42 +0800

    添加 mac .gitconfig

---
+++ cfg_git/git-diff-wrapper-bc-mac.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# diff is called by git with 7 parameters:
+# path old-file old-hex old-mode new-file new-hex new-mode
+#echo "Beyond Compare 3 prepare to compare below files :"
+#echo "path : $1"
+#echo "old-file : $2"
+#echo "old-hex : $3"
+#echo "new-file : $5"
+#echo "new-hex : $6"
+"/Applications/Beyond Compare.app/Contents/MacOS/bcomp" "$2" "$5" | cat
