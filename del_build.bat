c48e921a1d1b1511edff7b8046175573ecdd8d9f
Guang1234567 <lihanseygmail@gmail.com>
2018-05-25 15:15:10 +0800

    * update

---
+++ del_build.bat
@@ -0,0 +1,14 @@
+@echo on  
+@setlocal enabledelayedexpansion  
+  
+@REM 设置你想删除的目录  
+@set WHAT_SHOULD_BE_DELETED=build 
+  
+for /r . %%a in (!WHAT_SHOULD_BE_DELETED!) do (  
+  if exist %%a (  
+  echo "删除"%%a   
+  rd /s /q "%%a"  
+ )  
+ )  
+  
+@pause
